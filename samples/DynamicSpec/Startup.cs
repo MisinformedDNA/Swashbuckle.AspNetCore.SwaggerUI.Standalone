8a548ae6ab39f37d1a1e51cbbb1a0ede0808dcaf
Dan Friedman <dan@thefriedmans.org>
2020-05-07 09:55:11 -0500

    Update samples

---
+++ samples/DynamicSpec/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.AzureFunctions.Extensions;
+using SwashBuckle.AspNetCore.SwaggerUI.Standalone;
+using System.Reflection;
+
+[assembly: FunctionsStartup(typeof(DynamicSpec.Startup))]
+namespace DynamicSpec
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+
+            var functionAssembly = Assembly.GetExecutingAssembly();
+            services.AddAzureFunctionsApiProvider(functionAssembly);
+
+            // Add Swagger Configuration
+            services.AddSwaggerGen(options =>
+            {
+                // SwaggerDoc - API
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Example API",
+                    Version = "v1",
+                    Description = "Example API",
+                });
+            });
+
+            builder.Services.AddTransient<SwaggerUIFileService>();
+            builder.Services.AddSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/api/swagger.json", "v1");
+            });
+        }
+    }
+}
---
+++ samples/DynamicSpec/SwaggerFunctions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.AzureFunctions.Annotations;
+using Swashbuckle.AspNetCore.AzureFunctions.Extensions;
+using SwashBuckle.AspNetCore.SwaggerUI.Standalone;
+using System;
+using System.Net.Http;
+
+namespace DynamicSpec
+{
+    public class SwaggerFunctions
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly SwaggerUIFileService _swaggerService;
+
+        public SwaggerFunctions(IServiceProvider serviceProvider
+            , SwaggerUIFileService swaggerService
+            )
+        {
+            _serviceProvider = serviceProvider;
+            _swaggerService = swaggerService;
+        }
+
+        [FunctionName("GetSwaggerSpecFunction")]
+        [SwaggerIgnore]
+        public HttpResponseMessage GetSwaggerSpec(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "swagger.json")] HttpRequestMessage req,
+            ILogger log)
+        {
+            var content = _serviceProvider.GetSwagger("v1");
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(content)
+            };
+        }
+
+        [FunctionName("ViewSwaggerSpecFunction")]
+        [SwaggerIgnore]
+        public HttpResponseMessage GetSwaggerStaticFile(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "swagger/{path?}")] HttpRequestMessage req,
+            string path,
+            ILogger log)
+        {
+            return _swaggerService.GetFileResponse(path);
+        }
+    }
+}
